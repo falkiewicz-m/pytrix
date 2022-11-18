161a0d9f1ce4dabe98aa1818efee14c8fac8cf82
Michal Falkiewicz <michal.falkiewicz.m@gmail.com>
2022-11-18 23:19:31 +0100

    Adding notebook and label

--- pytrix.py
+++ pytrix.py
@@ -14,14 +14,38 @@ class Window:
                          padding=[10, 10, 10, 10],
                          font="Verdana 12 underline")
 
-        bttn = ttk.Button(frame, text="test", style="Custom.TButton", state="focus")
+        bttn = ttk.Button(frame, text="test", style="Custom.TButton", state="disabled")
         bttn.pack()
 
         frame.pack(padx = 5, pady = 5)
 
+        style.configure("Custom.TLabel",
+        				foreground="magenta",
+        				background="yellow",
+        				padding=[10, 10, 10,10],
+        				font="Helvetica 15")
+        label1 = ttk.Label(frame, text="text on label", style="Custom.TLabel")
+        label1.pack()
+        frame.pack(padx = 200, pady = 10)
+
         bttn2 = tk.Button(text="second button with another style", fg="red", bg="white")
         bttn2.pack()
 
+        notebook1 = ttk.Notebook(root)
+        ntbframe1 = ttk.Frame(notebook1, width="500", height="400")
+        ntbframe2 = ttk.Frame(notebook1, width="1020", height="780")
+        label2 = ttk.Label(ntbframe1, text="Label in notebook", style="Custom.TLabel")
+        label2.pack()
+        ntbframe1.pack(expand="True")
+        ntbframe2.pack(expand="True")
+
+        notebook1.add(ntbframe1, text="First notebook tab")
+        notebook1.add(ntbframe2, text="There is another")
+        notebook1.pack()
+
+
+
 root = tk.Tk()
+root.title("Tkinter experiments")
 window = Window(root)
 root.mainloop()
